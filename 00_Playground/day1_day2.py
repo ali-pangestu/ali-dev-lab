d42c2c357e90b03cfb5caa47d5d80116b2476ae6
Muhammad Ali Pangestu <alipangestu@proton.me>
2025-12-01 23:41:44 +0300

    Selesai materi Day 1 dan Day 2

---
+++ 00_Playground/day1_day2.py
@@ -0,0 +1,15 @@
+# --- MATERI DAY 1 (MINGGU) ---
+print("--- LATIHAN DAY 1 ---")
+print("Halo GitHub, maaf kemarin saya sibuk!")
+print(2025 - 1990) # Contoh hitung matematika
+
+# --- MATERI DAY 2 (SENIN) ---
+print("\n--- LATIHAN DAY 2 (VARIABLES) ---")
+# Variabel itu kayak kotak sepatu yang dikasih label
+nama = "Ali Pangestu"
+pekerjaan = "Future AI Expert"
+gaji_target = 5000 
+
+print(f"Nama: {nama}")
+print(f"Target: {pekerjaan}")
+print(f"Gaji USD: {gaji_target}")
