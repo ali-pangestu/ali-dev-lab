edbc3806ca7b2823ac367372c2b291ca0db578b3
Muhammad Ali Pangestu <alipangestu@proton.me>
2025-12-19 23:58:15 +0300

    paham materi variabel

---
+++ 00_Playground/iseng.py
@@ -0,0 +1,11 @@
+# 1. Isi awal kotak
+tabungan = 50
+print("Tabungan awal :" , tabungan)
+
+# 2. Update isinya (Re-assignment)
+tabungan = 100
+print("Tabungan sekarang" , tabungan)
+
+# 3. Update lagi
+tabungan = tabungan + 50
+print("Tabungan total" , tabungan)
