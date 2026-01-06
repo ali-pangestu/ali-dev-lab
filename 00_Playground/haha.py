d35707f065392e22fdd1d02d417a9ad703d9d324
Muhammad Ali Pangestu <alipangestu@proton.me>
2026-01-06 13:39:11 +0300

    start lagi

--- 00_Playground/haha.py
+++ 00_Playground/haha.py
@@ -5,3 +5,5 @@ jam_kerja = 04.00
 print("Jam berapa berangkat kerja, Sayang?" , jam_kerja)
 print("telatt woii")
 print("With God, this time my turn. Letss Gooo!!")
+
+print("Hii Im back, sorry my focuss is missing")
